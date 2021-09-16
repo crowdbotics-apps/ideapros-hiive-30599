99493c0e2e50bc7a34c6a07e941eec920c76a156
Crowdbotics <team@crowdbotics.com>
2021-09-16 13:20:42 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "ideapros_hiive_30599Identifier",
+  url: "https://ideapros_hiive_30599Identifier.botics.co",
+  api: "https://ideapros_hiive_30599Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
